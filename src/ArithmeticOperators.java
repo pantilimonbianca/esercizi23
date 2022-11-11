028db446746d73c05960383d097260010b5a8c51
pantilimonbianca <pantilimonbianca00@gmail.com>
2022-11-11 13:05:16 +0100

    final commit

---
+++ src/ArithmeticOperators.java
@@ -0,0 +1,49 @@
+
+
+/**
+ * @author  Bianca Pantilimon
+ * This class rappresents arithmetisc operation
+ */
+public class ArithmeticOperators {
+
+
+    /**
+     * Sum two number a and b
+     * @param a  first number
+     * @param b  second number
+     * @return results of operation
+     */
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Subtract b number from a number
+     * @param a first number
+     * @param b second number
+     * @return results of operation
+     */
+    public int sub(int a, int b){
+        return a - b;
+    }
+
+    /**
+     * Multiply the value a for the b value
+     * @param a  first number
+     * @param b second number
+     * @return results of operations
+     */
+    public int mult(int a, int b){
+        return a * b;
+    }
+
+    /**
+     * divides the first number by the second
+     * @param a  first number
+     * @param b second number
+     * @return results of operation
+     */
+    public int div(int a, int b){
+        return a/b;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,21 @@
+public class Test {
+
+    /**
+     * main method
+     *
+     * @param args console args
+     */
+    public static void main(String[] args) {
+
+        ArithmeticOperators a = new ArithmeticOperators();
+
+        System.out.println("Sum: " + a.sum(9, 8));
+        System.out.println("Sub: " + a.sub(80, 3));
+        System.out.println("Mul: " + a.mult(55, 2));
+        System.out.println("Sub operation: " + a.div(6, 2));
+
+
+
+    }
+}
+
